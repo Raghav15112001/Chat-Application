b7d9926cebaf5a372a80328408a9cbb53807aa2d
Raghav Shah <raghavshah900@gmail.com>
2022-08-30 15:05:47 +0530

    Add Flask-wtf for registration page

---
+++ wtforms_fields.py
@@ -0,0 +1,15 @@
+from email import message
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import InputRequired, Length, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    """ Registration form """
+
+    username = StringField('username_label',validators=[InputRequired(message="Username Required"),
+    Length(min=4,max=25, message="Usename must be between 4 to 25 characters")])
+    password = PasswordField('password_label',validators=[InputRequired(message="Password Required"),
+    Length(min=4,max=25, message="Password must be between 4 to 25 characters")])
+    confirm_pswd = PasswordField('confirm_pswd_label',validators=[InputRequired(message="Password Required"),EqualTo('password',message="Password must match")])
+    submit_button = SubmitField('Create')
